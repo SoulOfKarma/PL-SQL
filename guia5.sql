5cb4a461077c37cb229da19fdf013dc332722c0a
Ricardo Soto <gomez.soto.ricardo@gmail.com>
2017-05-23 14:38:51 -0400

    Com lel

---
+++ guia5.sql
@@ -0,0 +1,29 @@
+SET SERVEROUTPUT ON;
+DECLARE
+CURSOR cur_alumnos_ASIG IS 
+SELECT COD_ALUMNO,SNOMBRE_ALUMNO||' '||PNOMBRE_ALUMNO nombrealumno
+FROM ALUMNO;
+CURSOR cur_asig_prog(cod_alum NUMBER) IS 
+SELECT asi.NOMBRE,prom.SITUACION_ASIG FROM PROMEDIO_ASIG_ALUMNO prom JOIN
+ASIGNATURA asi ON(prom.COD_ASIGNATURA=asi.COD_ASIGNATURA)
+WHERE prom.COD_ALUMNO = cod_alum;
+v_number NUMBER(3):=0;
+BEGIN
+DBMS_OUTPUT.PUT_LINE(' Listado Situacion De Asignaturas de los ALumnos ');
+DBMS_OUTPUT.PUT_LINE(RPAD(' '),25,'-');
+DBMS_OUTPUT.NEW_LINE();
+DBMS_OUTPUT.PUT_LINE(RPAD(' ORDEN',7,' ')
+||RPAD(' ALUMNO',15,' ')
+||RPAD(' ASIGNATURA',15,' ')
+||RPAD(' SITUACION',10,' '));
+DBMS_OUTPUT.PUT_LINE(RPAD(' ',50,'-'));
+ FOR reg_alumnos IN cur_alumnos LOOP
+   FOR reg_prom_asi IN cur_asig_prog(reg_alumnos.COD_ALUMNO)LOOP
+     
+  
+ END LOOP;
+END;
+
+select* from alumno;
+select* from PROMEDIO_ASIG_ALUMNO;
+SELECT * FROM ASIGNATURA;
