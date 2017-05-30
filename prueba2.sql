6eaf94a7dd6dab913e65b2480ead3a5d57a72610
Ricardo Soto <gomez.soto.ricardo@gmail.com>
2017-05-30 17:15:14 -0400

    Com lel

--- prueba2.sql
+++ prueba2.sql
@@ -1,88 +1,56 @@
 SET SERVEROUTPUT ON;
+
 DECLARE 
-CURSOR c_emp IS 
-SELECT NUMRUT_EMP,SUELDO_EMP,ID_CATEGORIA_EMP cod_cat FROM EMPLEADO;
-CURSOR c_pro_arr(NROPRO NUMBER) IS
-SELECT SUM(p.VALOR_ARRIENDO)conta FROM PROPIEDAD p 
-JOIN ARRIENDO_PROPIEDAD arr
-ON(p.NRO_PROPIEDAD=arr.NRO_PROPIEDAD)
-WHERE p.NUMRUT_EMP=NROPRO;
-v_number NUMBER(7);
-v_porc NUMBER(2);
-BEGIN
- FOR reg_emp IN c_emp LOOP
-  v_number := 0;
-  
-  FOR reg_pro_arr IN c_pro_arr(reg_emp.NUMRUT_EMP)LOOP
-  
-  IF reg_pro_arr.conta  BETWEEN 1 AND 200000 AND reg_emp.cod_cat = 3 THEN
-  v_number := reg_pro_arr.conta * 0.10;
-  ELSIF reg_pro_arr.conta BETWEEN 200001 AND 300000 AND reg_emp.cod_cat = 3 THEN
-  v_number := reg_pro_arr.conta * 0.15;
-   ELSIF reg_pro_arr.conta BETWEEN 300001 AND 500000 AND reg_emp.cod_cat = 3 THEN
-  v_number := reg_pro_arr.conta * 0.20;
-   ELSIF reg_pro_arr.conta BETWEEN 500001 AND 850000 AND reg_emp.cod_cat = 3 THEN
-  v_number := reg_pro_arr.conta * 0.25;
-   ELSIF reg_pro_arr.conta BETWEEN 850001 AND 1000000 AND reg_emp.cod_cat = 3 THEN
-  v_number := reg_pro_arr.conta * 0.30;
-   ELSIF reg_pro_arr.conta BETWEEN 1000001 AND 2000000 AND reg_emp.cod_cat = 3 THEN
-  v_number := reg_pro_arr.conta * 0.35;
-  END IF;
-  DBMS_OUTPUT.PUT_LINE(
-  RPAD(reg_emp.NUMRUT_EMP,12,' ')
-  ||RPAD(v_number,10,' ')
-  ||RPAD(reg_emp.SUELDO_EMP,10,' ')
-  ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.085),10,' ')
-  ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.097),10,' ')
-  ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.1345),10,' ')
-  ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.0655),10,' ')
-  ||RPAD(reg_emp.cod_cat,10,' '));
-  END LOOP; 
- END LOOP;
- EXCEPTION
- WHEN NO_DATA_FOUND THEN
- DBMS_OUTPUT.PUT_LINE(' ');
- WHEN TOO_MANY_ROWS THEN
- DBMS_OUTPUT.PUT_LINE(' ');
- WHEN OTHERS THEN
- DBMS_OUTPUT.PUT_LINE(' ');
-END;
-
-
-
-SET SERVEROUTPUT ON;
-DECLARE 
-CURSOR c_emp IS 
-SELECT NUMRUT_EMP,SUELDO_EMP,ID_CATEGORIA_EMP FROM EMPLEADO;
-CURSOR c_pro_arr(NROPRO NUMBER) IS
-SELECT SUM(p.VALOR_ARRIENDO)conta FROM PROPIEDAD p 
-JOIN ARRIENDO_PROPIEDAD arr
-ON(p.NRO_PROPIEDAD=arr.NRO_PROPIEDAD)
-WHERE p.NUMRUT_EMP=NROPRO 
-AND ((arr.FECINI_ARRIENDO < to_date('01/05/2016','dd/mm/yy')
- and nvl(arr.FECTER_ARRIENDO,to_date('01/06/3016','dd/mm/yy'))
-> (to_date('01/06/2016','dd/mm/yyyy')-1))
-or to_char(arr.FECINI_ARRIENDO,'YYYYMM') = '201605'
-or to_char(arr.FECTER_ARRIENDO,'YYYYMM') = '201605'               
-);
+ cursor c_emp is select * from empleado;
+    reg_emp c_emp%rowtype;
+v_cant NUMBER (9);
 v_number NUMBER(7);
 v_porc NUMBER(2):= 0;
+mes_pro NUMBER(1) :=5;
+anio_proceso NUMBER(4) := 2016;
+v_errdes VARCHAR2(255);
 BEGIN
- FOR reg_emp IN c_emp LOOP
-  v_number := 0;
-BEGIN
-  FOR reg_pro_arr IN c_pro_arr(reg_emp.NUMRUT_EMP)LOOP
+
+  open c_emp;
+    loop
+        fetch c_emp into reg_emp;
+        exit when c_emp%notfound;
+        
+        DBMS_OUTPUT.PUT_line('Nombre :' || reg_emp.nombre_emp);
+
+        v_number := 0;
+        IF reg_emp.ID_CATEGORIA_EMP = 3 THEN 
+         BEGIN
+         select sum(pro.valor_arriendo) into v_cant
+         from EMPLEADO emp JOIN PROPIEDAD pro
+         ON(emp.NUMRUT_EMP=pro.NUMRUT_EMP)JOIN arriendo_propiedad arr
+         ON(pro.NRO_PROPIEDAD=arr.NRO_PROPIEDAD)
+         where emp.NUMRUT_EMP= reg_emp.NUMRUT_EMP AND((arr.FECINI_ARRIENDO < to_date('01/05/2016','dd/mm/yyyy')
+         and nvl(arr.FECTER_ARRIENDO,to_date('01/06/2016','dd/mm/yyyy'))
+         >(to_date('01/06/2016','dd/mm/yyyy')-1))
+         or to_char(arr.FECINI_ARRIENDO,'YYYYMM') = '201605'
+         or to_char(arr.FECTER_ARRIENDO,'YYYYMM') = '201605');
      
-  v_porc := 0;
+         SELECT PORC_COMISION INTO v_porc FROM PORCENTAJE_COMISION 
+         WHERE v_cant > TOTAL_ARRIENDO_INF
+         AND v_cant < TOTAL_ARRIENDO_SUP;
+         v_number :=  ((v_cant * v_porc)/100); 
+
+        EXCEPTION
+         WHEN NO_DATA_FOUND THEN
+         v_errdes := SQLERRM;
+         DBMS_OUTPUT.PUT_LINE('No tiene *******  ' || reg_emp.NUMRUT_EMP);
+         INSERT INTO ERROR_CALC_REMUN(CORREL_ERROR,RUTINA_ERROR,DESCRIP_ERROR)
+         VALUES(seq_error.nextval,'No se encuentra registro','CODIGO: '||' MENSAJE:'|| v_errdes);
+         v_porc := 0;
+         when others then
+         DBMS_OUTPUT.PUT_LINE('ERRRRORRRRRRR *******....:):):):):):):)  ' ||reg_emp.NUMRUT_EMP);                           
+         v_porc := 0;
+   
+        END;
  
-   IF reg_emp.ID_CATEGORIA_EMP = 3 THEN 
-    
-   SELECT PORC_COMISION INTO v_porc FROM PORCENTAJE_COMISION 
-   WHERE reg_pro_arr.conta > TOTAL_ARRIENDO_INF
-    AND reg_pro_arr.conta < TOTAL_ARRIENDO_SUP;
-    
-     v_number :=  ((reg_pro_arr.conta * v_porc)/100); 
-    DBMS_OUTPUT.PUT_LINE(
+        END IF;
+ /*  DBMS_OUTPUT.PUT_LINE(
     RPAD(reg_emp.NUMRUT_EMP,12,' ')
     ||RPAD(v_number,10,' ')
     ||RPAD(reg_emp.SUELDO_EMP,10,' ')
@@ -90,28 +58,26 @@ BEGIN
     ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.097),10,' ')
     ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.1345),10,' ')
     ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.0655),10,' ')
-    ||RPAD(reg_emp.ID_CATEGORIA_EMP,10,' '));
-   ELSE 
-    DBMS_OUTPUT.PUT_LINE(
-    RPAD(reg_emp.NUMRUT_EMP,12,' ')
-    ||RPAD(v_number,10,' ')
-    ||RPAD(reg_emp.SUELDO_EMP,10,' ')
-    ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.085),10,' ')
-    ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.097),10,' ')
-    ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.1345),10,' ')
-    ||RPAD(ROUND(reg_emp.SUELDO_EMP*0.0655),10,' '));
-   END IF;
-  END LOOP; 
-    EXCEPTION
- WHEN NO_DATA_FOUND THEN
- DBMS_OUTPUT.PUT_LINE(' 1');
- WHEN TOO_MANY_ROWS THEN
- DBMS_OUTPUT.PUT_LINE(' 1');
- WHEN OTHERS THEN
- DBMS_OUTPUT.PUT_LINE(' 1');
-  END;
+    ||RPAD(reg_emp.ID_CATEGORIA_EMP,10,' ')
+    ||RPAD(v_cant,10,' '));
+    */
+    INSERT INTO HABERES VALUES
+    (reg_emp.NUMRUT_EMP,
+    mes_pro,
+    anio_proceso,
+    reg_emp.SUELDO_EMP,
+    v_number,
+    ROUND(reg_emp.SUELDO_EMP*0.085),
+    ROUND(reg_emp.SUELDO_EMP*0.097));
+    INSERT INTO DESCUENTOS VALUES
+    (reg_emp.NUMRUT_EMP,
+    mes_pro,
+    anio_proceso,
+    ROUND(reg_emp.SUELDO_EMP*0.1345),
+    ROUND(reg_emp.SUELDO_EMP*0.0655));
+  END LOOP;
 
- END LOOP;
+ 
  EXCEPTION
  WHEN NO_DATA_FOUND THEN
  DBMS_OUTPUT.PUT_LINE(' 1');
@@ -119,11 +85,12 @@ BEGIN
  DBMS_OUTPUT.PUT_LINE(' 1');
  WHEN OTHERS THEN
  DBMS_OUTPUT.PUT_LINE(' 1');
+ 
+close c_emp;
 END;
-
-
-
-
-
-
---
+/*
+SELECT * FROM DESCUENTOS;
+SELECT * FROM HABERES;
+SELECT * FROM ERROR_CALC_REMUN;
+TRUNCATE TABLE HABERES;
+TRUNCATE TABLE DESCUENTOS;*/
